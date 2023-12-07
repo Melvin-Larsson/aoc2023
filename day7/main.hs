bda6bc6ec056f6bad29a1bdb88a33651ce05790b
Melvin-Larsson <melvin.larsson@hotmail.se>
2023-12-07 07:34:27 +0100

    Improve Day 7

--- day7/main.hs
+++ day7/main.hs
@@ -2,18 +2,11 @@ import Data.List
 import Data.Char
 
 
-data Hand = Hand Type Int [Label] deriving (Show, Eq)
+data Hand = Hand Type [Label] Int deriving (Show, Eq, Ord)
 
 data Type = High | One | Two | Three | FullHouse | Four | Five deriving (Show, Eq ,Ord)
 
 data Label = Numeric Int | T | J | Q | K | A deriving (Show, Eq, Ord)
-
-instance Ord Hand
-    where
-        (<=) (Hand t1 _ s1) (Hand t2 _ s2) = t1 < t2 || (t1 == t2 && diff1 < diff2)
-            where
-                (diff1, diff2) = (head $ dropWhile (\(n1, n2) -> n1 == n2) $ zip s1 s2) 
-
 
 main :: IO ()
 main = do
@@ -28,7 +21,7 @@ part1 = score compare . parse getLabelCount1
 part2 = score comp . parse getLabelCount2
 
 score :: (Hand -> Hand -> Ordering) -> [Hand] -> Int
-score c = sum . map (\(n,(Hand  _ v _)) -> n * v) . zip [1..] . sortBy c
+score c = sum . zipWith (\n (Hand  _ _ v) -> n * v) [1..] . sortBy c
 
 
 getLabelCount1 :: String -> [Int]
@@ -44,11 +37,11 @@ getLabelCount2 s
         (c:cs) = count
 
 comp :: Hand -> Hand -> Ordering
-comp (Hand t1 _ s1) (Hand t2 _ s2)
-  | t1 /= t2 = compare t1 t2
-  | diff1 == J    = LT
-  | diff2 == J    = GT
-  | otherwise = compare diff1 diff2
+comp (Hand t1 s1 _) (Hand t2 s2 _)
+  | t1 /= t2   = compare t1 t2
+  | diff1 == J = LT
+  | diff2 == J = GT
+  | otherwise  = compare diff1 diff2
     where
         (diff1, diff2) = (head $ dropWhile (\(n1, n2) -> n1 == n2) $ zip s1 s2) 
 
@@ -57,20 +50,19 @@ parse :: (String -> [Int]) -> String -> [Hand]
 parse f = map (parseRow f) . lines
 
 parseRow :: (String -> [Int]) -> String -> Hand
-parseRow f s = Hand (getType f t) (read num) (map getLabel t)
+parseRow f s = Hand (getType f t) (map getLabel t) (read num)
     where
         t = takeWhile (/= ' ') s
         num = tail $ dropWhile (/= ' ') s
 
 getLabel :: Char -> Label
-getLabel c
-  | isDigit c = Numeric (ord c - ord '0')
 getLabel c = case c of
     'A' -> A
     'K' -> K
     'Q' -> Q
     'J' -> J
     'T' -> T
+    _  -> Numeric (ord c - ord '0')
 
 getType :: (String -> [Int]) -> String -> Type
 getType f s = case f s of
@@ -81,5 +73,3 @@ getType f s = case f s of
     [2, 2, 1]    -> Two
     [2, 1, 1, 1] -> One
     [1,1,1,1,1]  -> High
-
-
