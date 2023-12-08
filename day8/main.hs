eec75b9aafcfff92a8ed0c8d09dca080617efedb
Melvin-Larsson <melvin.larsson@hotmail.se>
2023-12-08 08:55:59 +0100

    Removed unecessary stuff

--- day8/main.hs
+++ day8/main.hs
@@ -7,10 +7,6 @@ data Node = Node
         right :: String
      } deriving Eq
 
-instance Show Node
-  where
-    show (Node name _ _ ) = name
-
 data Command = Left | Right deriving (Show, Eq)
 
 main :: IO ()
@@ -22,7 +18,7 @@ main = do
     putStrLn $ show $ part2 input
 
 part1 :: String -> Int
-part1 s = length $ walkToEnd ((==) "ZZZ" . name) (cs, ns) start
+part1 s = length $ walkToEnd ((==) "ZZZ" . name) ns cs start
   where
     (cs, ns) = parse s
     start = head $ filter ((==) "AAA" . name) ns
@@ -30,18 +26,18 @@ part1 s = length $ walkToEnd ((==) "ZZZ" . name) (cs, ns) start
 part2 :: String -> Int
 part2 = walkToEnd2 . parse
 
+
+
+walkToEnd :: (Node -> Bool) -> [Node] -> [Command] -> Node -> [Node]
+walkToEnd isEnd ns (c:cs) start
+    | isEnd start   = []
+    | otherwise = start : walkToEnd isEnd ns (cs ++ [c]) (next ns start c)
+
 walkToEnd2 :: ([Command], [Node]) -> Int
 walkToEnd2 (cs, ns) = foldr lcm 1 lengthsToEnds
     where
         starts = filter ((==) "A" . drop 2 . name) ns
-        lengthsToEnds = map (length . walkToEnd ((==) "Z" . drop 2 . name) (cs, ns)) starts
-
-walkToEnd :: (Node -> Bool) -> ([Command], [Node]) -> Node -> [Node]
-walkToEnd isEnd (cs, ns) start = walkToEnd' ns cs start
-    where
-        walkToEnd' ns (c:cs) n
-          | isEnd n   = []
-          | otherwise = n : walkToEnd' ns (cs ++ [c]) (next ns n c)
+        lengthsToEnds = map (length . walkToEnd ((==) "Z" . drop 2 . name) ns cs) starts
 
 
 next :: [Node] -> Node -> Command -> Node
@@ -49,6 +45,7 @@ next (n1@(Node name1 left1 right1):xs) n2@(Node name2 left2 right2) command
   | command == Left && name1 == left2 = n1
   | command == Right && name1 == right2 = n1
   | otherwise = next xs n2 command
+
 
 
 parse :: String -> ([Command], [Node])
@@ -66,14 +63,5 @@ parseNode s = Node name left right
     where
         name = takeWhile (/= ' ') s
         leftRight = tail $ init $ dropWhile (/= '(') s
-        [left, right] = splitOn ", " leftRight
-
-
-splitOn :: Eq a => [a] -> [a] -> [[a]]
-splitOn = splitOn' []
-    where
-        splitOn' :: Eq a => [a] -> [a] -> [a] -> [[a]]
-        splitOn' aq _ [] = [aq]
-        splitOn' aq c ss@(x:xs)
-            | take (length c) ss == c = aq : splitOn' [] c (drop (length c) ss)
-            | otherwise               = splitOn' (aq ++ [x]) c xs
+        left = take 3 leftRight
+        right = drop 5 leftRight
